f1c2ddbb472d40bb4d453136f143214e0031ca4d
shadstoneofficial <contact@shadstone.com>
2026-05-15 07:40:57 +0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://handshake.org',
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap({
+    filter: (page) => page !== 'https://handshake.org/skill.md' && page !== 'https://handshake.org/llms.txt'
+  })]
 });
