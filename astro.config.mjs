a65e3c4c0f7528276bfdd520b086001486db2719
shadstoneofficial <contact@shadstone.com>
2026-05-15 10:16:40 +0700

    logo asset file and more historical context

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,11 @@ export default defineConfig({
   })],
   redirects: {
     '/discord': 'https://discord.gg/Vq3PWF6cJ6',
-    '/telegram': 'https://t.me/handshake_hns'
+    '/telegram': 'https://t.me/handshake_hns',
+    '/claim.html': '/claim',
+    '/faq.html': '/faq',
+    '/community.html': '/community',
+    '/grant-sponsors.html': '/grant-sponsors',
+    '/how-it-works.html': '/how-it-works'
   }
 });
